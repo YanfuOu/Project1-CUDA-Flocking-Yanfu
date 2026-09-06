450e27efd6d6ed98ced2631305761ca261a2d323
yanfu-ou-linux <yanfuou2@seas.upenn.edu>
2026-09-05 22:33:31 -0400

    part 1.2 Rule1 Cohesion done

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,3 +1,4 @@
+#include "glm/detail/func_geometric.hpp"
 #define GLM_FORCE_CUDA
 
 #include <cuda.h>
@@ -248,9 +249,23 @@ void Boids::copyBoidsToVBO(float *vbodptr_positions, float *vbodptr_velocities) 
 */
 __device__ glm::vec3 computeVelocityChange(int N, int iSelf, const glm::vec3 *pos, const glm::vec3 *vel) {
   // Rule 1: boids fly towards their local perceived center of mass, which excludes themselves
+  glm::vec3 perceived_center(0.0f, 0.0f, 0.0f); 
+  unsigned num_neighbors = 0; 
+  for(int i = 0; i < N; i++) {
+    if(i != iSelf && glm::distance(pos[i], pos[iSelf]) < rule1Distance) {
+      perceived_center += pos[i]; 
+      num_neighbors++; 
+    }
+  }
+  // in the special case of isolated boids that has 0 neighbors, we want to prevent divide by 0 error 
+  if (num_neighbors == 0) {
+    return glm::vec3(0.0f); 
+  }
+  perceived_center /= (float)num_neighbors; 
+  return (perceived_center - pos[iSelf]) * rule1Scale; 
   // Rule 2: boids try to stay a distance d away from each other
   // Rule 3: boids try to match the speed of surrounding boids
-  return glm::vec3(0.0f, 0.0f, 0.0f);
+  // return glm::vec3(0.0f, 0.0f, 0.0f); // this is the default 
 }
 
 /**
@@ -259,9 +274,20 @@ __device__ glm::vec3 computeVelocityChange(int N, int iSelf, const glm::vec3 *po
 */
 __global__ void kernUpdateVelocityBruteForce(int N, glm::vec3 *pos,
   glm::vec3 *vel1, glm::vec3 *vel2) {
-  // Compute a new velocity based on pos and vel1
-  // Clamp the speed
-  // Record the new velocity into vel2. Question: why NOT vel1?
+  int idx = blockIdx.x * blockDim.x + threadIdx.x; 
+  if(idx < N) {
+    // Compute a new velocity based on pos and vel1
+    glm::vec3 delta_v = computeVelocityChange(N, idx, pos, vel1); 
+    glm::vec3 new_v = vel1[idx] + delta_v; 
+    // Clamp the speed
+    float speed = glm::length(new_v); 
+    if(speed > maxSpeed) {
+      new_v *= maxSpeed / speed; 
+    }
+    // Record the new velocity into vel2. Question: why NOT vel1?
+    vel2[idx] = new_v; 
+  }
+
 }
 
 /**
@@ -365,7 +391,11 @@ __global__ void kernUpdateVelNeighborSearchCoherent(
 */
 void Boids::stepSimulationNaive(float dt) {
   // TODO-1.2 - use the kernels you wrote to step the simulation forward in time.
+  dim3 fullBlocksPerGrid((numObjects + blockSize - 1) / blockSize);
+  kernUpdateVelocityBruteForce<<<fullBlocksPerGrid, blockSize>>>(numObjects, dev_pos, dev_vel1, dev_vel2); 
+  kernUpdatePos<<<fullBlocksPerGrid, blockSize>>>(numObjects, dt, dev_pos, dev_vel2); 
   // TODO-1.2 ping-pong the velocity buffers
+  std::swap(dev_vel1, dev_vel2); 
 }
 
 void Boids::stepSimulationScatteredGrid(float dt) {
